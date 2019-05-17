0db156063c64b19855d22dd1076e33542a380f0d
Shelby Doolittle <shelby@shelbyd.com>
2019-05-17 14:56:10 -0400

    move loop outside of work loop

--- src/tree_search.rs
+++ src/tree_search.rs
@@ -6,7 +6,10 @@ pub trait TreeSpace: Sync {
 
     fn initial(&self) -> Box<Iterator<Item = Self::Candidate>>;
 
-    fn following<'s>(&'s self, candidate: Self::Candidate) -> Box<Iterator<Item = Self::Candidate> + 's>;
+    fn following<'s>(
+        &'s self,
+        candidate: Self::Candidate,
+    ) -> Box<Iterator<Item = Self::Candidate> + 's>;
 
     fn each(&self, candidate: &Self::Candidate);
 }
@@ -18,8 +21,8 @@ pub fn tree_search<S: TreeSpace>(space: S) {
     let undone = Mutex::new(undone);
     rayon::scope(|s| {
         for _ in 0..threads {
-            s.spawn(|_| {
-               work_loop(&space, &undone);
+            s.spawn(|_| loop {
+                work_loop(&space, &undone);
             });
         }
     });
@@ -27,13 +30,12 @@ pub fn tree_search<S: TreeSpace>(space: S) {
 
 fn work_loop<S: TreeSpace>(space: &S, work_queue: &Mutex<FastDeque<S::Candidate>>) {
     let amount = 1024;
-    loop {
-        let work = work_queue.lock().unwrap().split_off(amount);
-        let new_items = work.into_iter()
-            .flat_map(|v| v)
-            .inspect(|c| space.each(c))
-            .flat_map(|c| space.following(c))
-            .collect::<Vec<_>>();
-        work_queue.lock().unwrap().give(new_items);
-    }
+    let work = work_queue.lock().unwrap().split_off(amount);
+    let new_items = work
+        .into_iter()
+        .flat_map(|v| v)
+        .inspect(|c| space.each(c))
+        .flat_map(|c| space.following(c))
+        .collect::<Vec<_>>();
+    work_queue.lock().unwrap().give(new_items);
 }
